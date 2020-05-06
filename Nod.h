13c7ccc8c59145b2f0b8eda28f35c7765e176485
GabrielaIon <62449131+GabrielaIon@users.noreply.github.com>
2020-05-06 16:14:57 +0300

    Create Nod.h

---
+++ Nod.h
@@ -0,0 +1,23 @@
+#ifndef UNTITLED13_NOD_H
+#define UNTITLED13_NOD_H
+
+class Nod {
+protected:
+    int info;
+    Nod *left;
+    Nod *right;
+public:
+    Nod(int info = 0, Nod *left = nullptr, Nod *right = nullptr);
+    virtual ~Nod();
+    int get_info();
+    void set_info(int n_info);
+
+    virtual Nod *get_left();
+    void set_left(Nod *n_left);
+
+    virtual Nod *get_right();
+    void set_right(Nod *n_right);
+};
+
+
+#endif //UNTITLED13_NOD_H
