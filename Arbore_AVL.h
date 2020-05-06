4d9d58acff035dfcb7b3952bcbfca748489155ff
GabrielaIon <62449131+GabrielaIon@users.noreply.github.com>
2020-05-06 16:18:19 +0300

    Create Arbore_AVL.h

---
+++ Arbore_AVL.h
@@ -0,0 +1,30 @@
+#ifndef UNTITLED13_ARBORE_AVL_H
+#define UNTITLED13_ARBORE_AVL_H
+
+
+#include "Nod_AVL.h"
+#include "Arbore.h"
+#include "ABC.h"
+
+class Arbore_AVL : public ABC{
+    Nod_AVL *root;
+public:
+    explicit Arbore_AVL(Nod_AVL *root = nullptr);
+    ~Arbore_AVL() override;
+    void clear();
+    int balance(Nod_AVL *n);
+    void rebalance(Nod_AVL *n);
+    void rotate(Nod_AVL* n);
+    Nod_AVL * LLR(Nod_AVL* cur);
+    Nod_AVL * LRR(Nod_AVL* cur);
+    Nod_AVL * RRR(Nod_AVL* cur);
+    Nod_AVL * RLR(Nod_AVL* cur);
+    void printInOrder(Nod_AVL* n) const;
+    void printPostOrder(Nod_AVL* n) const;
+    void printPreOrder(Nod_AVL* n) const;
+    void print_balance(Nod_AVL *curr, std::ostream &out) const;
+    friend std::ostream &operator<< (std::ostream &out, Arbore_AVL arb);
+};
+
+
+#endif //UNTITLED13_ARBORE_AVL_H
